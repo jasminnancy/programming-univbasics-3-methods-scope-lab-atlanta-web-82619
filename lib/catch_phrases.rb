203d8101881d1b959838587c45582d91ebab3625
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-14 17:20:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,6 +9,10 @@ def toadstool
 end
 
 def link
-  phrase = "It's dangerous to go alone! Take This."
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+def any_phrase(phrase)
   puts phrase
 end
